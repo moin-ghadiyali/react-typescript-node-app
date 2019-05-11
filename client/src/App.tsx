a292a9447736a467300b4ad8447ac5a4dc2d51d3
mdmoin786313 <mdmoin786313@gmail.com>
2019-05-11 08:57:39 +0530

    comments deleted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,12 +8,10 @@ state = {
   };
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
     this.callBackendAPI()
       .then(res => this.setState({ data: res.express }))
       .catch(err => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
     const response = await fetch('/express_backend');
     const body = await response.json();
