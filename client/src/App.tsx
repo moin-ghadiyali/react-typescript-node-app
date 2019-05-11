56c2f67afd01e2fe464791e8358b121707c44ed1
teams-sc <teams.staticcodes@gmail.com>
2019-05-11 20:15:48 +0530

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
 state = {
@@ -24,12 +22,8 @@ state = {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
+      <div>
+        <p>{this.state.data}</p>
       </div>
     );
   }
